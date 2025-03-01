888dc42fdbc8f5ed83b7e30c96ed2812cf7d0149
kanulakmal <arkanbaggot111@gmail.com>
2025-03-01 17:04:59 +0700

    Update bot_binance.py

--- bot_binance.py
+++ bot_binance.py
@@ -34,6 +34,8 @@ scheduler.add_job(autotrade, 'interval', minutes=15)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
+    scheduler.print_jobs()
     scheduler.start()
     print("Scheduler is running...")
     app.run(host='0.0.0.0', port=port)
+
