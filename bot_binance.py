86211513d667c73a29e5127669c3302042a4a9bd
kanulakmal <arkanbaggot111@gmail.com>
2025-03-01 17:13:07 +0700

    Update bot_binance.py

--- bot_binance.py
+++ bot_binance.py
@@ -18,24 +18,23 @@ import datetime
 
 app = Flask(__name__)
 
-scheduler = BackgroundScheduler()
+# Fungsi Autotrade
+def autotrade():
+    print(f"Running Autotrade... {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    # Logic Autotrade Buy dan Sell disini
 
 @app.route('/')
 def home():
     return "Hello, Bot Binance AI is Online!"
 
-# Fungsi Autotrade
-def autotrade():
-    now = datetime.datetime.now()
-    print(f"Running Autotrade... {now.strftime('%Y-%m-%d %H:%M:%S')}")
-
-# Jadwalin bot setiap 15 menit
+# Jadwalin bot tiap 15 menit
+scheduler = BackgroundScheduler()
 scheduler.add_job(autotrade, 'interval', minutes=15)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    scheduler.print_jobs()
     scheduler.start()
     print("Scheduler is running...")
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port) # Fix port for Render
+
 
